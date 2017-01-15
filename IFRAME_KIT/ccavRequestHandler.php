854e48d7ab86b4771cbb61c324c0e216bd82dc40
eksubin <subinedu@gmail.com>
2017-01-16 00:55:38 +0530

    a

---
+++ IFRAME_KIT/ccavRequestHandler.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+<title> Iframe</title>
+</head>
+<body>
+<center>
+<?php include('Crypto.php')?>
+<?php 
+
+	error_reporting(0);
+
+	$working_key='391C34ABC8DD370712F2AB0E160F18E2';//Shared by CCAVENUES
+	$access_code='AVBZ68EA76BA61ZBAB';//Shared by CCAVENUES
+	$merchant_data='121148';
+
+	
+	foreach ($_POST as $key => $value){
+		$merchant_data.=$key.'='.$value.'&';
+	}
+	
+	$encrypted_data=encrypt($merchant_data,$working_key); // Method for encrypting the data.
+
+	$production_url='https://test.ccavenue.com/transaction/transaction.do?command=initiateTransaction'.$encrypted_data.'&access_code='.$access_code;
+?>		
+<iframe src="<?php echo $production_url?>" id="paymentFrame" width="482" height="450" frameborder="0" scrolling="No" ></iframe>
+
+<script type="text/javascript" src="jquery-1.7.2.js"></script>
+<script type="text/javascript">
+    	$(document).ready(function(){
+    		 window.addEventListener('message', function(e) {
+		    	 $("#paymentFrame").css("height",e.data['newHeight']+'px'); 	 
+		 	 }, false);
+	 	 	
+		});
+</script>
+</center>
+</body>
+</html>
+
