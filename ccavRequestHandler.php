5d6b30ff9e8ce69f06be8948cc7ffb3545ca4990
eksubin <subinedu@gmail.com>
2017-01-16 01:13:22 +0530

    data6

---
+++ ccavRequestHandler.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+<title> Non-Seamless-kit</title>
+</head>
+<body>
+<center>
+
+<?php include('Crypto.php')?>
+<?php 
+
+	error_reporting(0);
+	
+	$merchant_data='121148';
+	$working_key='391C34ABC8DD370712F2AB0E160F18E2';//Shared by CCAVENUES
+	$access_code='AVBZ68EA76BA61ZBAB';//Shared by CCAVENUES
+	
+	foreach ($_POST as $key => $value){
+		$merchant_data.=$key.'='.$value.'&';
+	}
+
+	$encrypted_data=encrypt($merchant_data,$working_key); // Method for encrypting the data.
+
+?>
+<form method="post" name="redirect" action="https://test.ccavenue.com/transaction/transaction.do?command=initiateTransaction"> 
+<?php
+echo "<input type=hidden name=encRequest value=$encrypted_data>";
+echo "<input type=hidden name=access_code value=$access_code>";
+?>
+</form>
+</center>
+<script language='javascript'>document.redirect.submit();</script>
+</body>
+</html>
+
